d7d670c18ef9917c0904dc2deafe8b365cabe7c7
EliasJuk <eliasp.juk@hotmail.com>
2022-09-30 17:05:58 -0300

    criar metodos da classe Sacola

--- src/main/java/me/dio/sacola/model/Sacola.java
+++ src/main/java/me/dio/sacola/model/Sacola.java
@@ -6,7 +6,7 @@ import me.dio.sacola.enumeration.FormaPagamento;
 import javax.persistence.*;
 import java.util.List;
 
-
+@Entity
 public class Sacola {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -18,12 +18,70 @@ public class Sacola {
 
     @OneToMany(cascade = CascadeType.ALL)
     private List<Item> itens;
-    private Double valoTotal;
+    private Double valorTotal;
 
     @Enumerated
     private FormaPagamento formaPagamento;
     private boolean fechada;
 
     public Sacola() {
+        //HIBERNATE
+    }
+
+    public Sacola(Long id, Cliente cliente, List<Item> itens, Double valorTotal, FormaPagamento formaPagamento, boolean fechada) {
+        this.id = id;
+        this.cliente = cliente;
+        this.itens = itens;
+        this.valorTotal = valorTotal;
+        this.formaPagamento = formaPagamento;
+        this.fechada = fechada;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Item> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<Item> itens) {
+        this.itens = itens;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public FormaPagamento getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(FormaPagamento formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public boolean isFechada() {
+        return fechada;
+    }
+
+    public void setFechada(boolean fechada) {
+        this.fechada = fechada;
     }
 }

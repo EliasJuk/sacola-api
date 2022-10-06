f2c021ef032ca0aa55c3e4735ef33042b1b276d7
EliasJuk <eliasp.juk@hotmail.com>
2022-10-06 17:28:54 -0300

    extends jparepository

--- src/main/java/me/dio/sacola/repository/ItemRepository.java
+++ src/main/java/me/dio/sacola/repository/ItemRepository.java
@@ -1,5 +1,9 @@
 package me.dio.sacola.repository;
 
-public interface ItemRepository {
+import me.dio.sacola.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }
